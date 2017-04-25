9fa4dc7c0d513dd04787d23c69f9aeab1fd70325
FranLucchini <flucchini@uc.cl>
2017-04-25 13:59:12 -0300

    readme

---
+++ appV2.js
@@ -0,0 +1,92 @@
+var express = require('express')
+var fs = require('fs');
+var busboy = require('connect-busboy');
+var bodyParser = require('body-parser')
+var shortid = require('shortid');
+var request = require("request");
+
+var app = express()
+var port =  process.env.HOST || "3000";
+
+var prefixDir = 'kattis-problemtools/problemtools/';
+var suffixDir = '/submissions/accepted/';
+var verificationScript = 'verifyproblem.py'
+var results = [];
+
+app.use(busboy());
+app.use(bodyParser());
+
+app.get('/', function (req, res) {
+  res.send('Done!')
+})
+
+app.listen(port, function () {
+  console.log('Example app listening on port '+port+'!')
+})
+
+/* TODO: Make Login submission
+*/
+app.post('/login', function(req, res) {
+  results = [];
+  res.send('Logged in!')
+})
+
+/* TODO: Make logout
+*/
+
+/*
+* Make program submission
+*/
+app.post('/submit', function(req, res) {
+    var fstream ;
+    var problemName = req.headers.problem;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+        console.log("Uploading: " + filename);
+
+        var name = shortid.generate();
+        var path = prefixDir + problemName + suffixDir
+        var problemDir = prefixDir + problemName;
+        console.log(path);
+
+        fstream = fs.createWriteStream(path + name + ".py");
+        file.pipe(fstream);
+
+        fstream.on('close', function () {
+          run_program(path, name, problemDir);
+        });
+        res.send("Evaluating...");
+    });
+});
+
+/* TODO: Run python program
+*/
+function run_program(path, script_name, problemName)
+{
+    var PythonShell = require('python-shell');
+    var options = {
+    mode: 'text',
+    pythonPath: 'python',
+    pythonOptions: ['-u'],
+    scriptPath: prefixDir,
+    args: [problemName, '-s', script_name]
+    };
+
+    var pyshell = new PythonShell(verificationScript, options);
+
+
+    pyshell.on('message', function (message) {
+      // received a message sent from the Python script (a simple "print" statement)
+      console.log(message);
+      results.push(message);
+    });
+
+    pyshell.end(function (err) {
+      if (err) throw err;
+      console.log('finished');
+    });
+
+    request("http://www.sitepoint.com", function(error, response, body) {
+      console.log(body);
+    });
+}
--- kattis-problemtools/problemtools/mult/submissions/accepted/mult.py
+++
@@ -1,9 +0,0 @@
-from sys import stdin
-
-line = stdin.readline()
-num = int(line)
-
-for i in range(0,num):
-    line = stdin.readline()
-    (n, m) = map(int, line.split())
-    print n*m
